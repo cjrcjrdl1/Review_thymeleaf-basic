8714a38173a502c7745c899b3e00a68d01e7aba4
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-06-07 19:55:26 +0900

    URL 링크

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -57,6 +57,19 @@ public class BasicController {
         return "basic/basic-objects";
     }
 
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+
     @Component("helloBean")
     static class HelloBean {
         public String hello(String data) {
